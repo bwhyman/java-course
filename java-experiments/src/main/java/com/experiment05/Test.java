39359fe6465a91e29a31acb6deb0df5d7efacaed
BO <bwang0629@gmail.com>
2021-04-23 23:45:54 +0800

    exp05

--- java-experiments/src/main/java/com/experiment05/Test.java
+++ java-experiments/src/main/java/com/experiment05/Test.java
@@ -15,21 +15,25 @@ public class Test {
 
     /**
      * 基于给定学生集合，学生编号，实现获取学生的导师的所在学院名称。
-     * 任何一项不存在或失败，打印显式 未知学院
-     * @param student
+     * 任何一项不存在或失败，返回 未知学院
+     * 结合collection stream findFirst()方法实现
+     * 可直接通过一条方法链实现
+     * @param students
      * @param sNumber
      * @return
      */
-    private static String getCourseName(List<Student> student, int sNumber) {
+    private static String getCollegeName(List<Student> students, int sNumber) {
        return null;
     }
 
     /**
      * 实现在给定学生集合中查询指定编号学生，如果学生导师编号为指定编号，打印显式导师所在学院名称。
      * 任何一项不存在或失败，打印显式 未知学院
+     * 结合collection stream findFirst()方法实现
+     * 可直接通过一条方法链实现
      * @param students
-     * @param sNumber
-     * @param tNumber
+     * @param sNumber，学生编号
+     * @param tNumber，教师编号
      */
     private static void printCollegeName(List<Student> students, int sNumber, int tNumber) {
 
